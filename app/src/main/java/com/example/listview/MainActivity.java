bc4b2a6366f3d05dc23566231a292aefd7488391
oanh0444@gmail.com <anhnguyetsol97@gmail.com>
2021-05-04 12:05:21 +0700

    Oanh: lan 2

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -13,12 +15,20 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView lvMonHoc;
+    Button btnThem;
+    EditText edtMOnHoc;
     ArrayList<String> arrayCourse;
+    Button btnCapNhat;
+    int vitri = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //ánh xạ
+        btnCapNhat = (Button) findViewById(R.id.buttonCapNhat);
+        btnThem = (Button) findViewById(R.id.buttonThem);
+        edtMOnHoc = (EditText) findViewById(R.id.editTextMonHoc);
         lvMonHoc = (ListView) findViewById(R.id.listviewMonHoc);
         arrayCourse = new ArrayList<>();
 
@@ -51,6 +61,47 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        // bắt sự kiện ấn nút btnThem
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String monhọc = edtMOnHoc.getText().toString(); // lấy nội dung từ editMonHoc
+                // add vào mảng Môn học
+                arrayCourse.add(monhọc);
+                //cập nhật lại adepter
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+        // bắt sự kiện đổ nội dung từng item lên edit text sau khi click
+        lvMonHoc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                edtMOnHoc.setText(arrayCourse.get(position));
+                vitri = position;
+            }
+        });
+        // bắt sự kiện ấn button Cập Nhật
+        btnCapNhat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrayCourse.set(vitri, edtMOnHoc.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        // bắt sự kiện xóa item
+        lvMonHoc.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                arrayCourse.remove(position);
+                adapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+
+
     }
 
 }
